c0ab4867f3110b8f4be2a16a96b75bae754bd720
unknown <peyman.norozy@gmail.com>
2023-10-26 19:16:33 +0330

    this is the ex

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,25 @@
+import { StyleSheet, Text, View } from "react-native";
+import Title from "../components/Title";
+
 const GameScreen = () => {
-  return (<>peyman</>);
-}
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      {/* GUESS */}
+      <View>
+        <Text>Higher or lower?</Text>
+        {/* + - */}
+      </View>
+      {/* <View>LOG ROUNDs</View> */}
+    </View>
+  );
+};
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
